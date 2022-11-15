2f327b6e4e037fd7ca5493c9592b3a41326777db
Roboss11 <sam.knight0411@gmail.com>
2022-11-15 20:31:48 +0000

    Added Chess board UI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     // Create board
     let mut board: [Tile; 64] = read_fen_string("RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rbnkqbnr");
+
+    initialise_window();
 }
 
 fn read_fen_string(string : &str) -> [Tile; 64] {
@@ -59,23 +61,91 @@ fn read_fen_string(string : &str) -> [Tile; 64] {
 }
 
 #[derive(Debug, Copy, Clone)]
-struct Tile {
+pub struct Tile {
     color : Option<Colors>,
     piece : Option<Pieces>
 }
 
 #[derive(Debug, Copy, Clone)]
-enum Colors {
+pub enum Colors {
     White,
     Black
 }
 
 #[derive(Debug, Copy, Clone)]
-enum Pieces {
+pub enum Pieces {
     King,
     Queen,
     Rook,
     Knight,
     Bishop,
     Pawn
+}
+
+// GUI Stuff
+
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+pub fn initialise_window() {
+    let opengl = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new("Chess", [800, 800])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    
+    let mut app : App = App {
+        gl : GlGraphics::new(opengl)
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
+    }
+}
+
+struct App {
+    gl : GlGraphics
+}
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const BLACK: [f32; 4] = [0.484f32, 0.582f32, 0.363f32, 1.00f32]; // Black square color (actually green)
+        const WHITE: [f32; 4] = [0.929f32, 0.929f32, 0.832f32, 1.00f32]; // White square color (actually cream)
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(WHITE, gl);
+
+            let mut square : [f64 ; 4];
+            for x in 0..8 {
+                for y in 0..8 {
+                    if (y % 2 == x % 2) {continue;}
+                    square = rectangle::square((x * 100) as f64, (y * 100) as f64, 100f64);
+                    rectangle(BLACK, square, c.transform, gl);
+                }
+            }
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        return;
+    }
+
 }
