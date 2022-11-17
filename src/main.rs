772eb4d5cb6e9a49cbf5580f945245d345f2a35d
Roboss11 <sam.knight0411@gmail.com>
2022-11-17 15:33:50 +0000

    Refactored board into a 2D array

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,61 @@
 fn main() {
     // Create board
-    let mut board: [Tile; 64] = read_fen_string("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+    let mut board: [[Tile; 8]; 8] = read_fen_string("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
 
     initialise_window(&mut board);
 }
 
-fn read_fen_string(string : &str) -> [Tile; 64] {
+fn read_fen_string(string : &str) -> [[Tile; 8]; 8] {
     let white_piece_codes: [u8; 6] = [75, 81, 82, 78, 66, 80]; // ascii codes for "KQRNBP"
     let black_piece_codes: [u8; 6] = [107, 113, 114, 110, 98, 112]; // ascii codes for "kqrnbp"
-    let mut board : [Tile; 64] = [Tile {color : None, piece : None}; 64];
-    let mut index : usize = 0;
+    let mut board : [[Tile; 8]; 8] = [[Tile {color : None, piece : None}; 8]; 8];
+    let mut x_index : usize = 0;
+    let mut y_index : usize = 0;
     let characters: &[u8] = string.as_bytes();
     for i in 0..characters.len() {
         // If character is a number
         if (characters[i] >= 48) & (characters[i] <= 57) {
             // In ASCII, 48 is 0, so use a -48 offset
             // In ASCII, 57 is 9, which will never be used
-            index += (characters[i] - 48u8) as usize;
+            x_index += (characters[i] - 48u8) as usize;
             continue;
         } else
         if characters[i] == 47 {
+            y_index += 1;
+            x_index  = 0;
             continue;
         } else
         if white_piece_codes.contains(&characters[i]) {
-            board[index].color = Some(Colors::White);
+            board[x_index][y_index].color = Some(Colors::White);
 
             // Compares the character utf-8 code to the correct piece
             match characters[i] {
-                75 => board[index].piece = Some(Pieces::King {has_moved : false}),
-                81 => board[index].piece = Some(Pieces::Queen),
-                82 => board[index].piece = Some(Pieces::Rook {has_moved : false}),
-                78 => board[index].piece = Some(Pieces::Knight),
-                66 => board[index].piece = Some(Pieces::Bishop),
-                80 => board[index].piece = Some(Pieces::Pawn {has_moved : false, double_moved : false}),
+                75 => board[x_index][y_index].piece = Some(Pieces::King {has_moved : false}),
+                81 => board[x_index][y_index].piece = Some(Pieces::Queen),
+                82 => board[x_index][y_index].piece = Some(Pieces::Rook {has_moved : false}),
+                78 => board[x_index][y_index].piece = Some(Pieces::Knight),
+                66 => board[x_index][y_index].piece = Some(Pieces::Bishop),
+                80 => board[x_index][y_index].piece = Some(Pieces::Pawn {has_moved : false, double_moved : false}),
                 _ => () // This will never be reached
             }
 
-            index += 1;
+            x_index += 1;
         } else
         if black_piece_codes.contains(&characters[i]) {
-            board[index].color = Some(Colors::Black);
+            board[x_index][y_index].color = Some(Colors::Black);
 
             // Compares the character utf-8 code to the correct piece
             match characters[i] {
-                107 => board[index].piece = Some(Pieces::King {has_moved : false}),
-                113 => board[index].piece = Some(Pieces::Queen),
-                114 => board[index].piece = Some(Pieces::Rook {has_moved : false}),
-                110 => board[index].piece = Some(Pieces::Knight),
-                98  => board[index].piece = Some(Pieces::Bishop),
-                112 => board[index].piece = Some(Pieces::Pawn {has_moved : false, double_moved : false}),
+                107 => board[x_index][y_index].piece = Some(Pieces::King {has_moved : false}),
+                113 => board[x_index][y_index].piece = Some(Pieces::Queen),
+                114 => board[x_index][y_index].piece = Some(Pieces::Rook {has_moved : false}),
+                110 => board[x_index][y_index].piece = Some(Pieces::Knight),
+                98  => board[x_index][y_index].piece = Some(Pieces::Bishop),
+                112 => board[x_index][y_index].piece = Some(Pieces::Pawn {has_moved : false, double_moved : false}),
                 _ => () // This will never be reached
             }
 
-            index += 1;
+            x_index += 1;
         }
     }
 
@@ -119,6 +122,7 @@ extern crate opengl_graphics;
 extern crate piston;
 
 use std::path::Path;
+use std::thread::current;
 
 use opengl_graphics::{GlGraphics, OpenGL, Texture, TextureSettings};
 
@@ -132,7 +136,7 @@ use piston::window::WindowSettings;
 use graphics::rectangle::square;
 use graphics::Image;
 
-fn initialise_window(board: &mut[Tile; 64]) {
+fn initialise_window(board: &mut[[Tile; 8]; 8]) {
     let opengl = OpenGL::V3_2;
 
     let mut window: Window = WindowSettings::new("Chess", [800, 800])
@@ -155,11 +159,11 @@ fn initialise_window(board: &mut[Tile; 64]) {
         Texture::from_path(Path::new("./resources/BlackRook.png"), &texture_settings).unwrap(),
         Texture::from_path(Path::new("./resources/BlackPawn.png"), &texture_settings).unwrap()
     ];
-    let mut image_locations : [Image; 64] = [Image::new() ; 64];
+    let mut image_locations : [[Image; 8]; 8] = [[Image::new(); 8]; 8];
     // Creates the places where images can be drawn
     for y in 0..8 {
         for x in 0..8 {
-            image_locations[y * 8 + x] = Image::new()
+            image_locations[x][y] = Image::new()
                     .rect(square((x * 200) as f64, (y * 200) as f64, 200f64));
         }
     }
@@ -171,7 +175,7 @@ fn initialise_window(board: &mut[Tile; 64]) {
         selected_tile : None
     };
 
-    let mut mouse_position : [f64 ; 2] = [0f64, 0f64];
+    let mut mouse_position : [f64; 2] = [0f64, 0f64];
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
@@ -203,12 +207,12 @@ fn initialise_window(board: &mut[Tile; 64]) {
 
 struct App {
     gl : GlGraphics,
-    piece_images : [Texture ; 12],
-    image_locations : [Image ; 64],
-    selected_tile : Option<usize>
+    piece_images : [Texture; 12],
+    image_locations : [[Image; 8]; 8],
+    selected_tile : Option<[usize; 2]>
 }
 impl App {
-    fn render(&mut self, args: &RenderArgs, board : &[Tile; 64], mouse_position : &[f64 ; 2]) {
+    fn render(&mut self, args: &RenderArgs, board : &[[Tile; 8]; 8], mouse_position : &[f64; 2]) {
         use graphics::*;
 
         // Color constants
@@ -223,7 +227,7 @@ impl App {
             clear([0f32, 0f32, 0f32, 1f32], gl);
 
             // Draws the board squares
-            let mut square : [f64 ; 4];
+            let mut square : [f64; 4];
             for x in 0..8 {
                 for y in 0..8 {
                     square = rectangle::square((x * 100) as f64, (y * 100) as f64, 100f64);
@@ -240,8 +244,8 @@ impl App {
             // Highlights the selected tile
             if let Some(_) = self.selected_tile {
                 // Get coordinates of the tile
-                let y_position : f64 = (self.selected_tile.unwrap() >> 3) as f64 * 100f64;
-                let x_position : f64 = (self.selected_tile.unwrap() % 8) as f64 * 100f64;
+                let x_position : f64 = (self.selected_tile.unwrap()[0] * 100) as f64;
+                let y_position : f64 = (self.selected_tile.unwrap()[1] * 100) as f64;
                 // Draws the square selected as yellow
                 rectangle(YELLOW, rectangle::square(x_position, y_position, 100f64), c.transform, gl);
             }
@@ -254,36 +258,39 @@ impl App {
             let draw_state : DrawState = DrawState::new_alpha();
             // Creates a matrix transformation to scale down the images
             let piece_transform = c.transform.scale(0.5, 0.5);
-            for i in 0..64 {
-                image_index = board[i].get_piece_image_index();
-                if let Some(_) = image_index {
-                    self.image_locations[i].draw(&self.piece_images[image_index.unwrap()], &draw_state, piece_transform, gl)
+            for y in 0..8 {
+                for x in 0..8 {
+                    image_index = board[x][y].get_piece_image_index();
+                    if let Some(_) = image_index {
+                        self.image_locations[x][y].draw(&self.piece_images[image_index.unwrap()], &draw_state, piece_transform, gl)
+                    }
                 }
             }
         });
     }
 
-    fn update_selected_tile(&mut self, x_index : f64, y_index : f64, board : &mut[Tile ; 64]) {
-        let new_tile: usize = (y_index * 8f64 + x_index) as usize;
+    fn update_selected_tile(&mut self, x_index : f64, y_index : f64, board : &mut[[Tile; 8]; 8]) {
+        let y_index: usize = y_index as usize;
+        let x_index: usize = x_index as usize;
         match self.selected_tile {
             Some(_) => {
                 let current_tile = self.selected_tile.unwrap();
-                match board[current_tile].piece {
+                match board[current_tile[0]][current_tile[1]].piece {
                     Some(_) => {
-                        if board[current_tile].color != board[new_tile].color {
-                            board[new_tile] = board[current_tile];
-                            board[current_tile].piece = None;
-                            board[current_tile].color = None;
+                        if board[current_tile[0]][current_tile[1]].color != board[x_index][y_index].color {
+                            board[x_index][y_index] = board[current_tile[0]][current_tile[1]];
+                            board[current_tile[0]][current_tile[1]].piece = None;
+                            board[current_tile[0]][current_tile[1]].color = None;
                         }
                         self.clear_selected_tile();
                     },
                     None => {
-                        self.selected_tile = Some(new_tile);
+                        self.selected_tile = Some([x_index, y_index]);
                     }
                 }
             },
             None => {
-                self.selected_tile = Some(new_tile);
+                self.selected_tile = Some([x_index, y_index]);
             }
         }
     }
